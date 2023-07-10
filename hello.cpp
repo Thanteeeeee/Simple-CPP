68b68e651ba24a4591940061e8e5393acbafbd41
Thanteeeeee <139093769+Thanteeeeee@users.noreply.github.com>
2023-07-10 10:37:11 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{ cout<<"Student Code : 6606021633017"<<endl;
+  cout<<"Student Name : Thantheeradone Srivarom"<<endl;
+  cout<<"    Division : Information Technology"<<endl;
+  cout<<"        Year : 1"<<endl;
+  cout<<"        Room : RC"<<endl;
+  cout<<"         Sex : Male"<<endl;
+  cout<<"         Age : 17"<<endl;
+  system("pause");
+  return(0);
+}
